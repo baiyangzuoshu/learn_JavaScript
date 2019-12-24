8040296ec56d217dcc720fbbf999c09946ef5142
942357585 <942357585@qq.com>
2019-12-24 08:34:06 +0800

    优化Function

--- 字符串/Function练习.js
+++ 字符串/Function练习.js
@@ -11,12 +11,13 @@ function    sum(){
 }
 
 let sumc=new Function(
-    'let args=arguments;'+
-    'let sum=0;'+
-    'for(let i=0;i<args.length;i++){'+
-    '    sum+=args[i];'+
-    '}'+
-    'return sum;'
+    `let args=arguments;
+    let sum=0;
+    for(let i=0;i<args.length;i++){
+        sum+=args[i];
+    }
+    return sum;`
 );
 
 console.log(`sum:${sumc(1,2,3,4,5,6,7,8,9)}`);
+
